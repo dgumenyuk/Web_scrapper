02bfbdbf775f3ce16333abe20a7f8a2d21887b63
dgumenyuk <gumenyuk98@gmail.com>
2020-02-19 18:05:02 -0500

    Updated script
    Internal urls are united with the base address and checking is done with urllib

--- Final_code/Regex.py
+++ Final_code/Regex.py
@@ -1,52 +1,31 @@
 
-
 import requests
-from csv import writer
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import urllib.request
-import re
-from html.parser import HTMLParser
 
+import re
+#from html.parser import HTMLParser
+
+
+from urllib.parse import urlsplit
+
+url = "http://www.stallman.org"
+base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))  # getting domain name
+print(base_url)
 
 #http://ethans_fake_twitter_site.surge.sh/
-#response = requests.get('http://www.stallman.org')
-response = requests.get('https://en.wikipedia.org/wiki/Polytechnique_Montr%C3%A9al')
+response = requests.get('http://www.stallman.org')
+#response = requests.get('https://en.wikipedia.org/wiki/Polytechnique_Montr%C3%A9al')
 
 
 soup = str(BeautifulSoup(response.text, 'html.parser'))
 
-'''
-
-#print((str(soup)))
-p = re.findall('http(.*?)"', str(soup))
-#p = re.findall('http.+"', str(soup))
-
-#p = re.findall('(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', str(soup))
-print(len(p))
-
-for i in range(len(p)):
-    item = p[i]
-    # ... compute some result based on item ...
-    item = item + "\n"
-    print(item)
-'''
-
-#pattern = re.compile(r'="[a-zA-Z]*(://)(www\.)?\w+\.\w+')
 pattern = re.compile(r'href="(.*?)"')
 
-#pattern = re.compile(r'(http:\/)?(\/[\w\.\-]+)+\/?')
 
 matches = pattern.findall(soup)
 matches_obj = pattern.finditer(soup)
-result = ""
-end = 'w'
-i = 0
-#for (match, match_obj) in zip(matches, matches_obj):
-    #print(match)
-    
-
-#print(result)
-#[a-zA-Z0-9.]*"')
 
 
 internal =[]
@@ -58,9 +37,27 @@ for element in matches:
     
         external.append(element)
     else:
+        element =urljoin(base_url, element)
+        #element = urljoin("element")
         internal.append(element)
-        
+
+print("Relative links")
+for i in internal:
+    print(i)
+    try:
+        resp = urllib.request.urlopen(i)
+        if resp.code == 200:
+            print("WORKS")
+    except:
+        print("ERROR 404!!!")
+
+
+print("Absolute links")
 for i in external:
     print(i)
-    
-        
+    try:
+        resp = urllib.request.urlopen(i)
+        if resp.code == 200:
+            print("WORKS")
+    except:
+        print("ERROR 404!!!")
