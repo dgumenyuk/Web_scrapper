ebeb57a50506d6c836385890c9d0fb73169e32a2
dgumenyuk <gumenyuk98@gmail.com>
2020-02-21 23:11:02 -0500

    Final version

--- WEBSCRAPER/WEBSCRAPER.py
+++ WEBSCRAPER/WEBSCRAPER.py
@@ -1,3 +1,12 @@
+"""
+This is a program to find  broken links of the website.
+It gives the total number of links, number of internal, external, plain text and broken links.
+
+#Source:
+#https://github.com/dgumenyuk/Web_scrapper/tree/master/WEBSCRAPER
+
+"""
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
@@ -16,29 +25,35 @@ yellow = colorama.Fore.YELLOW
 magneta = colorama.Fore.MAGENTA
 reset = colorama.Fore.RESET
 
-internal = set()
-external = set()
-Plain_TEXT_Link = set()
+internal_url = set()
+external_url = set()
+plainText_url = set()
+broken_url = set()
 ALLURLS = set()
 
-count = 0
-
 def check(url):         # check the url in order to find the broken links
-    global count
+    #global count
 
     try:
         resp = urllib.request.urlopen(url, timeout=10)
         if resp.code == 200:
-            print(green + " working well " + url)
-    except HTTPError as e:
-        print(red + 'The server couldn\'t fulfill the request for the: ' + url)
+            print(green + url + "\nisn't broken")
+            print("\n")
+    except HTTPError as e:  # execute if the error occured
+        print(red + url + "\nis broken" + "\nWe failed to reach a server for the: " + url)
         print(red + 'Error code: ', e.code)
-        count += 1
+        ALLURLS.add(url)
+        broken_url.add(url)
+        print("\n")
+        #count += 1
 
     except URLError as e:
-        print(red + 'We failed to reach a server for the: ' + url)
+        print(red + url + "\nis broken" + "\nWe failed to reach a server for the: " + url)
         print(red + 'Reason: ', e.reason)
-        count += 1
+        ALLURLS.add(url)
+        broken_url.add(url)
+        print("\n")
+        #count += 1
 
 def is_valild(url):        # check the validity of the url using the url protocol and the domain
     parsed = urlparse(url)
@@ -46,96 +61,54 @@ def is_valild(url):        # check the validity of the url using the url protoco
 
 def crawler(url):          # crawl the webSite (url) to extract all the available links
     response = requests.get(url)
-    soup = str(BeautifulSoup(response.text, 'html.parser'))
-    base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))
-
-    urls = set()
+    soup = str(BeautifulSoup(response.text, 'html.parser'))  # get the html page as a string
+    base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))  # get the base URL (domain)
 
     pattern = re.compile(r'href="(.*?)"')
     matches = pattern.findall(soup)
 
-    for element in matches:                     # finding external links
+    for element in matches:                     # finding external_url links
         if element[0:4] == "http":
             ALLURLS.add(element)
             if is_valild(element):
-                external.add(element)
-                print(cyan + "External Link")
+                external_url.add(element)
+                print(cyan + "External link")
                 check(element)
 
-
-        else:                                    # finding internal links
+        else:                                    # finding internal_url links
             element = urljoin(base_url, element)
             ALLURLS.add(element)
             if is_valild(element):
-                internal.add(element)
-                print(cyan + "Internal Link")
+                internal_url.add(element)
+                print(cyan + "Internal link")
                 check(element)
 
 
+    pattern = re.compile(r' http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+[ .]')  # regex that finds all URLs in the plain text
+    matches = pattern.findall(soup)
 
-    for element in soup.split():                       # finding the links inside the body text outside of the special tags
-        if (element[0:4] == "http") and ("://" in element) and ("href") not in element:
-            path = urlparse(element).path
-            path = path.replace("~", ".")
-            path = path.replace("!", ".")
-            path = path.replace("@", ".")
-            path = path.replace("#", ".")
-            path = path.replace("$", ".")
-            path = path.replace("%", ".")
-            path = path.replace("^", ".")
-            path = path.replace("&", ".")
-            path = path.replace("*", ".")
-            path = path.replace("+", ".")
-            path = path.replace("=", ".")
-            path = path.replace("{", ".")
-            path = path.replace("}", ".")
-            path = path.replace("[", ".")
-            path = path.replace("]", ".")
-            path = path.replace(":", ".")
-            path = path.replace(";", ".")
-            path = path.replace("'", ".")
-            path = path.replace('"', '.')
-            path = path.replace("|", ".")
-            path = path.replace("<", ".")
-            path = path.replace(">", ".")
-            path = path.replace(",", ".")
-
-            if "." in path:
-                pattern = re.compile(r'(.*?)\.')
-                match = pattern.findall(path)
-                for each in match:
-                    if each != "":
-                        newPath = urlparse(element).scheme + "://" + urlparse(element).netloc + each
-                        ALLURLS.add(newPath)
-                        if is_valild(newPath):
-                            Plain_TEXT_Link.add(newPath)
-                            print(cyan + "Link in the body text")
-                            check(newPath)
-
-            else:
-                newPath = urlparse(element).scheme + "://" + urlparse(element).netloc + path
-                ALLURLS.add(newPath)
-                if is_valild(newPath):
-                    Plain_TEXT_Link.add(newPath)
-                    print(cyan + "Link in the body Text")
-                    check(newPath)
+    for element in matches:
+        if element not in ALLURLS:
+            ALLURLS.add(element)  # make sure it was not in internal or external links, then it must be in plain text
+            plainText_url.add(element)
+            print(cyan + "Plain text link")
+            check(element)
 
 
 if __name__ == "__main__":                             # main class to do the process
 
 
-    URL = input("please enter a url like http(s);//url  ")
+    URL = input("Please enter a url: ")
     crawler(URL)
 
     print(magneta + "Total number of the available links in the " + URL + " is " + str(len(ALLURLS)))
     print("=========================================================================================================")
-    print(gray + "Total number of the valid links = ", len(internal) + len(external) + len(Plain_TEXT_Link))
-    print(gray + "Total number of the valid internal links = ", len(internal))
-    print(gray + "Total number of the valid external links = ", len(external))
-    print(gray + "Total number of the valid links in the body text = ", len(Plain_TEXT_Link))
+    print(gray + "Total number of the valid links = ", len(internal_url) + len(external_url) + len(plainText_url))
+    print(gray + "Total number of the valid internal_url links = ", len(internal_url))
+    print(gray + "Total number of the valid external_url links = ", len(external_url))
+    print(gray + "Total number of the valid links in the body text = ", len(plainText_url))
     print("=========================================================================================================")
-    print(yellow + "Total number of the dead links (forbidden, not found, inaccessible) are ", count)
-
+    print(yellow + "Total number of the dead links (forbidden, not found, inaccessible) are ", len(broken_url))
 
 
 
