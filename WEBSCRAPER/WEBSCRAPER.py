463e2f72a09da6d40ddd5b8c3818e146f361542a
hoomanvhd <hooman_vahedi994@yahoo.ca>
2020-04-01 22:41:07 -0400

    a url or list of url done

--- WEBSCRAPER/WEBSCRAPER.py
+++ WEBSCRAPER/WEBSCRAPER.py
@@ -67,51 +67,50 @@ def link_extractor(url):          # crawl the webSite (url) to extract all the a
     for element in matches:
         element = urljoin(url, element)
 
-        if " " in element:  #this condition is just added in order to prevent from a bad link. (an example in the http://swat.polymtl.ca/index.html)
-            continue
+        try:
+            if not valild(element):               # check the validity of the link
+                continue
 
-        if not valild(element):               # check the validity of the link
-            continue
+            if element in internal_url:
+                continue
 
-        if element in internal_url:
-            continue
+            if base_url not in element:              # correction of prior script in order to reduce the duplication and check the external link without browsing them
+                if element not in external_url:
+                    print(cyan + "External Link")
+                    check(element)
+                    external_url.add(element)
+                continue
 
-        if base_url not in element:              # correction of prior script in order to reduce the duplication and check the external link without browsing them
-            if element not in external_url:
-                print(cyan + "External Link")
-                check(element)
-                external_url.add(element)
-            continue
-
-        print(cyan + "Internal Link")
-        check(element)                # check the internal link
-        urls.add(element)             # create urls to have the internal urls to crawl and browse them
-        internal_url.add(element)     # internal_link to have the number of the internal links and prevent from duplication
+                print(cyan + "Internal Link")
+                check(element)                # check the internal link
+                urls.add(element)             # create urls to have the internal urls to crawl and browse them
+                internal_url.add(element)     # internal_link to have the number of the internal links and prevent from duplication
+        except:
+            print(red + "there is a problem with the url: ", element)
 
     pattern = re.compile(r' http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+[ .]')  # regex that finds all URLs in the plain text
     matches = pattern.findall(soup)
 
     for element in matches:
         element = urljoin(url, element)
-        print(element)
 
-        if " " in element:
-            continue
-
-        if not valid(element):
-            continue
+        try:
+            if not valid(element):
+                continue
                                                 # same as the actions for the internal and external links
-        if element in internal_url:
-            continue
+            if element in internal_url:
+                continue
 
-        if element in external_url:
-            continue
+            if element in external_url:
+                continue
 
-        if base_url not in element:
-            if element not in plainText_url:
-                print(cyan + "Plain Text Link")
-                check(element)
-                plainText_url.add(element)
+            if base_url not in element:
+                if element not in plainText_url:
+                    print(cyan + "Plain Text Link")
+                    check(element)
+                    plainText_url.add(element)
+        except:
+            print(red + "there is a problem with the url: ", element)
 
     return urls
 
@@ -122,17 +121,19 @@ def crawler(url):                        # the crawler function crawls the websi
         crawler(link)
 
 
-def resultPrinter():
+def resultPrinter(url):
     print(gray + "=========================================================================================================")
-    print(gray + "Total number of the valid links = ", len(internal_url) + len(external_url) + len(plainText_url))
+    print(gray + "The result of scraping for the url: " + url)
+    print(gray + "\nTotal number of the valid links = ", len(internal_url) + len(external_url) + len(plainText_url))
     print(gray + "Total number of the valid internal_url links = ", len(internal_url))
     print(gray + "Total number of the valid external_url links = ", len(external_url))
     print(gray + "Total number of the valid links in the body text = ", len(plainText_url))
     print("=========================================================================================================")
     print(yellow + "Total number of the dead links (forbidden, not found, inaccessible) = ", len(broken_url))
 
-if __name__ == "__main__":                             # main class to do the process
 
+
+if __name__ == "__main__":                             # main class to do the process
 
 
     Specifier = ''
@@ -142,30 +143,46 @@ if __name__ == "__main__":                             # main class to do the pr
     conditionsCrawling = ('Y', 'N')
 
     while Specifier not in conditionsFileType:
-        Specifier = input(white + "Please specify the type of input method you want to use:\n\n1.a URL (html web page)\n2.a file containing list of urls (a .txt or .csv file)\n3.a list of website file (directories containing unlaunched websites files)\n\nPlease select one of the above options: [1/2/3] ")
+        Specifier = input(white + "Please specify the type of input method you want to use:\n\n1.a URL or multiple URLs\n2.a .txt file containing list of URLs\n3.an HTML page\n\nPlease select one of the above options: [1/2/3] ")
+
+
     if Specifier == '1':
         while CrawlingActivation.upper() not in conditionsCrawling:
             CrawlingActivation = input("\n" + white + "Do you want to keep the " + blue + "CRAWLING" + white + " Activated? [Y/n]  ")
-        URL = input("\nPlease enter a url: ")
+
+        regex = re.compile(r'^(?:http|ftp)s?://' # http:// or https://
+                           r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                           r'localhost|' #localhost...
+                           r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                           r'(?::\d+)?' # optional port
+                           r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        FinishingCondition = ('End, end, END, E, e')
+        URLlist = []
+
+        while True:
+            print(yellow + "\nif you want to finish the url enterring process please write " + blue + "end" + yellow + " and press Enter")
+            URL = input(white + "\nPlease enter a url: ")
+            if (re.match(regex, URL) is not None):
+                URLlist.append(URL)
+            if URL in FinishingCondition:
+                break
+
         CrawlingActivation = CrawlingActivation.upper()
         if CrawlingActivation == ("Y"):
-            crawler(URL)
-            resultPrinter()
+            for each in URLlist:
+                crawler(each)
+                resultPrinter(each)
         if CrawlingActivation == ("N"):
-            link_extractor(URL)
-            resultPrinter()
+            for each in URLlist:
+                link_extractor(each)
+                resultPrinter(each)
 
 
-    # if int(Specifier) == 2:
-    #     URL = input("\nPlease enter the address of the file (.txt or .csv) containing list of the urls: ")
-    #     while CrawlingActivation.upper() not in conditionsCrawling:
-    #         CrawlingActivation = input("\n" + white + "Do you want to keep the " + blue + "CRAWLING" + white + " Activated? [Y/n]  ")
-    #     CrawlingActivation = CrawlingActivation.upper()
-    #     if CrawlingActivation == ("Y"):
-    #         crawler(URL)
-    #     if CrawlingActivation == ("N"):
-    #         link_extractor(URL)
-    #
+
+
+
+
 
     if Specifier == 'BASH':
         print(yellow + "\nThis python script called from the Bash script to scrape the localhost webpage")
@@ -173,6 +190,6 @@ if __name__ == "__main__":                             # main class to do the pr
         try:
             link_extractor(URL)
             print(yellow + "\nThis python script called from the Bash script to scrape the localhost webpage")
-            resultPrinter()
+            resultPrinter(URL)
         except:
             print(yellow + "\nyou may not use a vaild localhost IP or PORT on the github repository")
