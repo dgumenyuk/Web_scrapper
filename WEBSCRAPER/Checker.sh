3d7a3ce84b7a4fd715a84b6752750bfd1cf132f8
dgumenyuk <gumenyuk98@gmail.com>
2020-04-03 19:19:14 -0400

    Adding comments

--- WEBSCRAPER/Checker.sh
+++ WEBSCRAPER/Checker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-
+#===============================================================================
+#
+#          FILE:  Checker.sh
+#
+#         USAGE:  ./Checker.sh -u <github url> -p <port number> [optional]
+#
+#   DESCRIPTION:  This script ckecks the web pages of the locally inastalled node server
+#         NOTES:  Link for testing: https://github.com/xileftenurb/polymtl-inf8007-site-exemple
+#        AUTHOR:  Dmytro Humeniuk and Mahmood Vahedi (dmytro.humeniuk@polymtl.ca)
+#       COMPANY:  Polytechnique Montreal
+#       VERSION:  1.0
+#       CREATED:  03/04/2020
+#===============================================================================
 
 function main(){
-	app_port=3000
-    name="git_server"
-	check $@
 
-    echo $url
-    server_install
+    app_port=3000  # save default port number
+    name="git_server"  # create a folder for storing the server
+    check $@  # check user input
+
+    server_install_check  #install and test the server
 
 
 }
@@ -16,30 +28,28 @@ function main(){
 function check(){
     while getopts p:u: opt; do
     	case "${opt}" in
-    		p) app_port=$OPTARG;;
+    	    p) app_port=$OPTARG;;
             u) url=$OPTARG;;
-            \?) help;;
         esac
     done
 
     if [ "$url" = "" ]; then
 
         echo "You did not supply a url!" 
-        #exit 1
-
+        exit 1
     fi
 
     if git ls-remote "$url" == 0; then
         printf '%s\n' "Your url is valid!"
     else 
         printf '%s\n' "$url does not exist" 
-        #exit 1
+        exit 1
     fi
 
 }
 
 
-function server_install(){
+function server_install_check(){
     git clone $url $name 
     cd $name 
     npm i 1> error.txt
